a0a32b0148da53fd575b22f3b98c198f04a0aa3d
MANOJ REDDY <manojreddy@MANOJs-Air.ht.home>
2022-10-13 10:23:09 -0400

    Initial Coomit

---
+++ COVID19/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace COVID19;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ COVID19/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace COVID19;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
